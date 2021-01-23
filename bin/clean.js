01101e025cb6ad5dcbf85e7da4bda1988fb7900c
Alexandre Le Foll <alexandre@le-foll.fr>
2021-01-23 18:39:55 +1100

    Version 0.1.0

--- bin/clean.js
+++
@@ -1,6 +0,0 @@
-require("ts-node").register({
-    project: "./tsconfig.tsnode.json",
-    transpileOnly: true,
-});
-
-module.exports = require("../tools/clean/cli.ts");
--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-require("ts-node").register({
-    project: "./tsconfig.tsnode.json",
-    transpileOnly: true,
-});
-
-module.exports = require("./webpack.config.ts");
