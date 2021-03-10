835ed5305b30acc12bb33569ea8819f701eb59eb
Alexandre Le Foll <alexandre@le-foll.fr>
2021-03-10 21:43:27 +1100

    Version 0.7.0

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -23,7 +23,7 @@ export class Calendar extends React.PureComponent<CalendarProps, CalendarState> 
         week: 0
     }
 
-    private readonly getVideos = (from: DateTime, to: DateTime) => {
+    private readonly getVideos = (from: DateTime, to: DateTime): VideoModel[] => {
         const users = this.props.users?.filter(user => user.videos?.length);
 
         const videos = users?.reduce((previous, user) => {
@@ -75,30 +75,31 @@ export class Calendar extends React.PureComponent<CalendarProps, CalendarState> 
         //     end: 53_000
         // }].map((_, index) => {
         //     return {
-        //         created_at: "2021-02-05T12:56:05Z",
+        //         copy: false,
+        //         created_at: "2021-03-08T12:56:05Z",
         //         description: "",
         //         duration: "5h44m54s",
         //         start_in_seconds: _.start,
         //         end_in_seconds: _.end,
-        //         id: index,
+        //         id: index.toString(),
         //         language: "fr",
         //         lineIndex: 0,
         //         overlap: 0,
-        //         published_at: "2021-02-05T12:56:05Z",
+        //         published_at: "2021-03-08T12:56:05Z",
         //         thumbnail_url: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==",
-        //         title: `${ index + 1 }eer Video - ${ index + 1 }e`,
+        //         title: `${ index }eer Video - ${ index }e`,
         //         type: "archive",
         //         url: "https://www.twitch.tv/videos/901834920",
         //         user_id: "40063341",
         //         user_login: "domingo",
         //         user_name: "Domingo",
         //         view_count: 167673,
-        //         viewable: "public"
+        //         viewable: "public",
         //     }
         // });
     }
 
-    private readonly getVideosByDay = (videos: VideoModel[]) => {
+    private readonly getVideosByDay = (videos: VideoModel[]): VideoModel[][] => {
         return videos.reduce((previous, video) => {
             const videoDate = DateTime.fromISO(video.created_at).setZone(Calendar.TIMEZONE);
 
@@ -131,86 +132,48 @@ export class Calendar extends React.PureComponent<CalendarProps, CalendarState> 
         }
     }
 
-    private readonly laConcu = (inputVideos: VideoModel[]): VideoModel[] => {
-        const videos = inputVideos.map(video => {
-            video.lineIndex = 0;
+    private readonly isBeetween = (number: number, min: number, max: number): boolean => {
+        return number > min && number < max;
+    }
 
-            return video;
-        });
+    private readonly laConcu = (inputVideos: VideoModel[]) => {
+        const videos: VideoModel[] = [];
 
-        const videosMetadata: VideoMetadata[] = [];
+        let maxLineIndex = 0;
 
-        for (let i = 0; i < videos.length; i++) {
-            const video = videos[i];
+        for (let i = 0; i < inputVideos.length; i++) {
+            const video = { ...inputVideos[i] };
 
-            const metadata = videosMetadata[i] || {
-                overlap: {
-                    start : [],
-                    end   : [],
-                    inner : [],
-                    outer : []
-                }
-            };
+            let found = false;
 
-            videos.map((videoToCheck) => {
+            for (let j = 0; j < videos.length; j++) {
+                const prevVideo = videos[j];
+
                 const a = video;
-                const b = videoToCheck;
+                const b = prevVideo;
 
-                if (a.id === b.id)
-                    return;
+                if (this.isBeetween(video.start_in_seconds, prevVideo.start_in_seconds, prevVideo.end_in_seconds)
+                 || this.isBeetween(video.end_in_seconds,   prevVideo.start_in_seconds, prevVideo.end_in_seconds)
+                 || (video.start_in_seconds < prevVideo.start_in_seconds && video.end_in_seconds > prevVideo.end_in_seconds)) {
+                    found = true;
 
-                if (a.start_in_seconds < b.start_in_seconds && a.end_in_seconds > b.end_in_seconds) {
-                    metadata.overlap.outer.push(b);
-                    return;
+                    if (video.lineIndex <= prevVideo.lineIndex) {
+                        video.lineIndex = prevVideo.lineIndex + 1;
+
+                        if (video.lineIndex > maxLineIndex) {
+                            maxLineIndex = video.lineIndex;
+                        }
+                    }
                 }
+            }
 
-                if (a.start_in_seconds > b.start_in_seconds && a.end_in_seconds < b.end_in_seconds) {
-                    metadata.overlap.inner.push(b);
-                    return;
-                }
-
-                if (a.start_in_seconds < b.start_in_seconds && b.start_in_seconds < a.end_in_seconds) {
-                    metadata.overlap.start.push(b);
-                    return;
-                }
-
-                if (b.start_in_seconds < a.start_in_seconds && a.start_in_seconds < b.end_in_seconds) {
-                    metadata.overlap.end.push(b);
-                    return;
-                }
-            });
-
-            // console.log(video.id + 1, metadata.overlap);
-
-            video.overlap = videos.length - 1;
-
-            video.lineIndex = i;
-
-            // if (metadata.overlap.start.length || metadata.overlap.inner.length) {
-            //     const fusion  = [...metadata.overlap.start, ...metadata.overlap.inner];
-            //     const indexes = fusion.map(_ => _.lineIndex);
-
-            //     let found = false;
-
-            //     for (let index = 0; index < fusion.length; index++) {
-            //         if (!found && !indexes.includes(index)) {
-            //             found = true;
-
-            //             video.lineIndex = index;
-            //         }
-            //     }
-
-            //     if (!found) {
-            //         video.lineIndex = Math.max(...indexes) + 1;
-            //     }
-            // }
-
-            videosMetadata.push(metadata);
+            videos.push(video);
         }
 
-        // videos.map(video => console.log(video.id + 1, video.title, video.overlap, video.lineIndex));
-
-        return videos;
+        return {
+            videos,
+            maxLineIndex: maxLineIndex + 1,
+        }
     }
 
     private setWeek = (week: number) => {
@@ -261,7 +224,8 @@ export class Calendar extends React.PureComponent<CalendarProps, CalendarState> 
         //     hourToShow = hourToShow.filter(hour => hour%2 === 0);
         // }
 
-        const vignette = 80;
+        const vignetteMarginTopBottom = 6;
+        const vignetteHeight = 80 + vignetteMarginTopBottom;
 
         // console.log('render');
 
@@ -284,11 +248,11 @@ export class Calendar extends React.PureComponent<CalendarProps, CalendarState> 
 
                     { videosByDay.map((videos, dayOfTheWeek) => {
                         // console.log(videos);
-                        const videosWithLineInfo = this.laConcu(videos);
+                        const { videos: videosWithLineInfo, maxLineIndex } = this.laConcu(videos);
                         // console.log(videosWithLineInfo);
 
                         return (
-                            <div key={ dayOfTheWeek } className="calendar--line" style={{ minHeight: (vignette * videosWithLineInfo.length) + "px" }}>
+                            <div key={ dayOfTheWeek } className="calendar--line" style={{ minHeight: ((vignetteHeight * maxLineIndex) + vignetteMarginTopBottom) + "px" }}>
                                 <div className="calendar--line__day">{ Info.weekdays("short",  { locale: Calendar.LOCALE })[dayOfTheWeek] }<br/>{ startOfWeek.plus({ day: dayOfTheWeek }).toFormat("dd/MM") }</div>
 
                                 <div className="calendar--line__content">
@@ -302,8 +266,7 @@ export class Calendar extends React.PureComponent<CalendarProps, CalendarState> 
                                                 left     : (((video.start_in_seconds - start) / (end - start)) * 100) + "%",
                                                 width    : (((video.end_in_seconds - video.start_in_seconds) / (end - start)) * 100) + "%",
                                                 minWidth : (((video.end_in_seconds - video.start_in_seconds) / (end - start)) * 100) + "%",
-                                                top      : ((100 / (video.overlap + 1)) * video.lineIndex) + "%",
-                                                height   : `calc(${ 100 / (video.overlap + 1) }% - ${ video.lineIndex === 0 ? 8 : 4 }px)`
+                                                top      : ((vignetteHeight * video.lineIndex) + vignetteMarginTopBottom) + "px"
                                             }
                                         }
 
