49b0c1425269f6ae670e5d15a35b347384fe8a4f
Alexandre Le Foll <alexandre@le-foll.fr>
2021-11-19 19:49:24 +1100

    Version 2.2.1

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import { SETTINGSKEY } from "@helpers/settings";
 
@@ -8,13 +9,19 @@ import fr from "./fr/translation.json";
 
 const settings = window.localStorage.getItem(SETTINGSKEY);
 
-i18n.use(initReactI18next).init({
-    lng: settings ? JSON.parse(settings).locale : undefined,
-    interpolation: {
-        escapeValue: false, // not needed for react as it escapes by default
-    },
-    resources: {
-        en,
-        fr,
-    },
-});
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        lng: settings ? JSON.parse(settings).locale : undefined,
+        detection: {
+            caches: [],
+        },
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+        resources: {
+            en,
+            fr,
+        },
+    });
